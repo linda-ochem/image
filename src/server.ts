1c34c7d42682d6c88aa22da37df6d520f6a21a33
linda <lindaochem@gmail.com>
2022-12-31 08:46:03 +0100

    initial

---
+++ src/server.ts
@@ -0,0 +1,106 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { requireAuth, generateJWT } from './auth'
+const path = require('path');
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  app.get('/filteredimage', requireAuth, async (req, res) => {
+    let image_URL: any = req.query.image_url;
+    //image_URL is type 'any' beacuse req.query.image_url can be string or object
+    //Takes the first image url in the event of same multiple queries
+    if (typeof image_URL === 'object') {
+      image_URL = image_URL[0]
+    }
+
+    //Check if query 'imageQual' exists
+    let qualityString: any = req.query.imageQual;
+    let quality: number;
+    //Set image quality limits 10 >= quality <= 100
+    if (qualityString) {
+      quality = parseInt(qualityString.toString());
+      if (quality < 10) { quality = 10; }
+      else if (quality >= 100) { quality = 100 }
+    } else {
+      quality = 60;
+    }
+
+    if (image_URL) {
+      if (image_URL !== '') {
+        //Prevents processing imageQual query as part of image url
+        let image__URL: string = image_URL.split('&imageQual=')[0]
+        await filterImageFromURL(image__URL, quality)
+          //if promise resolved
+          .then((filterImagePath) => {
+            res.status(200).sendFile(filterImagePath, () => {
+              deleteLocalFiles([filterImagePath])
+            })
+          })
+          //if promise rejected
+          .catch((error) => {
+            //console.log(error.message)
+            res.status(404).send(`Invalid Query Value. Could not fetch resource. Check image URL`)
+          })
+      } else {
+        //if image URL empty
+        res.status(406).send('Please enter image URL')
+      }
+    } else {
+      //if image_url query does not exist
+      res.status(400).send('Invalid Query Key. Try GET /filteredimage?image_url={{}}&imageQual={{1-100}}')
+    }
+  })
+
+  //Login, where token is generated from body parameters
+  app.post('/login', (req, res) => {
+    const { name, role } = req.body;
+    const tokenJWT: string = generateJWT({ name: name, role: role });
+    res.set('Authorisation', `Bearer ${tokenJWT}`)
+    res.send({ name: name, role: role, token: tokenJWT })
+  })
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user. Edited to add new query
+  app.get("/", (req, res) => {
+
+    res.send("try GET /filteredimage?image_url={{}}&imageQual={{1-100}}")
+
+  });
+
+  //Added by me: Ohaneme Kingsley. To redirect all other request types.
+  app.use((req, res) => {
+    res.redirect('/')
+  })
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
---
+++ src/util/util.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import Jimp = require("jimp");
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS modified by Ohamene Kingsley github.com/ohansck
+//    inputURL: string - a publicly accessible url to an image file
+//    imageQUality: number - an interger to set the desired output image quality
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+// Edited by Ohaneme Kingsley. User can set quality of filtered image.
+// Default quality set at 60
+export async function filterImageFromURL(inputURL: string, imageQuality: number): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const outpath =
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+        .resize(256, 256) // resize
+        .quality(imageQuality) // set JPEG quality. Edited by github.com/ohansck
+        .greyscale() // set greyscale
+        .write(__dirname + outpath, (img) => {
+          resolve(__dirname + outpath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
